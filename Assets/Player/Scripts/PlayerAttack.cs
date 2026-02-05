60555b20caf77218222f6bc85efe9b7346aa24b9
jamiemckenzie <jamiemckenzie.jm@googlemail.com>
2026-02-05 17:49:58 +0000

    Update PlayerAttack.cs
    Script For Player Attack written

--- Assets/Player/Scripts/PlayerAttack.cs
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -1,16 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Transform cameraTransform;
+    float range = 100f;
+
+    [SerializeField]
+    float rawDamage = 10f;
+
+
+    PlayerInput playerInput;
+    InputAction attackAction;
+
+    void OnEnable()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
+
+        var map = playerInput.currentActionMap;
+
+        attackAction = map.FindAction("Attack", true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        FireWeapon();
+    }
+
+    void FireWeapon()
+    {
+        if (attackAction.triggered)
+        {
+            cameraTransform = Camera.main.transform;
+            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit, range))
+            {
+                if (raycastHit.transform != null)
+                {
+                    raycastHit.collider.SendMessageUpwards("Hit", rawDamage, SendMessageOptions.DontRequireReceiver);
+
+                }
+            }
+            else
+            {
+                Debug.Log("NO RAYCAST");
+            }
+        }
     }
 }
